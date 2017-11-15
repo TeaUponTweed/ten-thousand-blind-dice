5f407c2ca1a89ac58179d2782d165a7e9c3e49f3
Michael Mason <TeaUponTweed@gmail.com>
2017-11-14 20:47:45 -0700

    make it work better

--- calc_white_area.py
+++ calc_white_area.py
@@ -7,12 +7,24 @@ if __name__ == '__main__':
     assert len(sys.argv) > 1
     img = cv2.imread(sys.argv[1])
     img = cv2.resize(img,(400,500))
+    print(img.shape)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret,gray = cv2.threshold(gray,127,255,0)
-    gray2 = gray.copy()
+    (_,a),(_,b),(_,c) = cv2.threshold(img[:, :, 0],127,255,0), cv2.threshold(img[:, :, 1],127,255,0), cv2.threshold(img[:, :, 2],127,255,0)
+    gray = a & b & c
     mask = np.zeros(gray.shape,np.uint8)
+    # print(mask)
     contours, hier = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        if 200<cv2.contourArea(cnt)<5000:
-            cv2.drawContours(img,[cnt],0,(0,255,0),2)
-            cv2.drawContours(mask,[cnt],0,255,-1)
+    def gen_contours(): 
+        for cnt in contours:
+            yield cv2.contourArea(cnt), cnt
+    _, cnt = max(gen_contours(), key=lambda x: x[0])
+    (x1, y1), (x2, y2), _ = cv2.fitEllipse(cnt)
+    print (x2-x1)/(y2-y1)
+
+    cv2.drawContours(img,[cnt],0,(0,255,0),2)
+    cv2.drawContours(mask,[cnt],0,255,-1)
+
+    # cv2.bitwise_not(gray2,gray2,mask)
+    cv2.imshow('IMG',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
